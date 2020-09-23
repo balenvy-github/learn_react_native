dc40cfdc5c7cd59505f52e294608c46b7f3cf3ac
iqbal w <iqbalwahyudi1843018@gmail.com>
2020-09-23 16:10:51 +0800

    first edit on App.js

--- App.js
+++ App.js
@@ -58,12 +58,6 @@ const App: () => React$Node = () => {
                 <DebugInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
             <LearnMoreLinks />
           </View>
         </ScrollView>
