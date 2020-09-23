8a2f53b63d6eba7ee2bf87cad76b5f6a1b99897d
iqbal w <iqbalwahyudi1843018@gmail.com>
2020-09-23 17:48:19 +0800

    day 1

--- App.js
+++ App.js
@@ -1,108 +1,131 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Image, Text, TextInput, View, StyleSheet} from 'react-native';
+import sampleImg from './sampleimg.jpg';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = () => {
+  return <StylingReactNativeComponent />;
+};
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const StylingReactNativeComponent = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+      <Text style={styles.text}>Styling Componenct</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#f0f',
+          borderWidth: 2,
+          borderColor: '#00f',
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+      <View
+        style={{
+          padding: 10,
+          backgroundColor: '#ccc',
+          width: 220,
+          marginTop: 20,
+        }}>
+        <Image
+          source={sampleImg}
+          style={{width: 200, height: 120, borderRadius: 8}}
+        />
+        <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
+          Card Title
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#f2994a',
+            marginTop: 12,
+          }}>
+          Rp. 25.000.000
+        </Text>
+        <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
+          Samarinda
+        </Text>
+        <View
+          style={{
+            backgroundColor: '#6fc',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: '#fff',
+              textAlign: 'center',
+            }}>
+            BELI
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  text: {
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+    marginTop: 40,
   },
 });
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: 'red'}} />
+      <Text>Tes</Text>
+      <Text>Tes2</Text>
+      <Text>Tes3</Text>
+      <Home />
+      <Photo />
+      <TextInput style={{borderWidth: 1}} />
+      <BoxGreen />
+      <Profile />
+    </View>
+  );
+};
+
+const Home = () => {
+  return <Text>Tes2 z</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/100/100/tech'}}
+      style={{width: 100, height: 100}}
+    />
+  );
+};
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini componen dari class</Text>;
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{uri: 'https://placeimg.com/100/100/animals'}}
+          style={{width: 100, height: 100}}
+        />
+        <Text style={{color: 'blue'}}>Ini kucing</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
